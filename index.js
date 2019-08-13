e7be4cd21c8c2b35100dd0e3ada72d913ba7e446
TANAKA-Makoto <45248369+TANAKA-Makoto@users.noreply.github.com>
2019-08-14 03:58:20 +0900

    echo

--- index.js
+++ index.js
@@ -2,17 +2,48 @@
 /*jshint node: true */
 "use strict";
 
+let argv = require('argv');
 let WebSocketServer = require('ws').Server;
-let port = 9000; //ポート番号は必要に応じて変更してください。
+let port; 
+//-------------------
+//  handle args
+//-------------------
+argv.option([
+{
+  name: 'port',
+  short: 'p',
+  type : 'Number',
+  description :'サーバのリッスンポート default:9000',
+  example: "'script --port=value' or 'script -p value'"
+},
+{
+  name: 'log',
+  short: 'l',
+  type : 'Number',
+  description :'log level "not work"',
+  example: ""
+}]);
+console.log(argv.run());
+port = argv.run()['options']['port'] || 9000;
+//-------------------
+
 let wsServer = new WebSocketServer({ port: port });
 console.log('websocket server start. port=' + port);
 
-wsServer.on('connection', function(ws) {
+wsServer.on('connection', function(ws, request) {
   console.log('-- websocket connected --');
+  const echo = is_echo(request['url']);
   ws.on('message', function(message) {
     console.log('receive message');
     console.log(message);
-    let msgObj = JSON.parse(message);
+    if(echo){ ws.send(message);}
+  });
+  ws.on('close',function(code, reason){
+    console.log(`client disconnect ${code}:${reason}`);
   });
 });
 
+function is_echo(url){
+  if (url ==='/echo') {return true;}
+  return false;
+}
